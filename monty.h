31640ec6c305d14dee9cf56db2f700aabe918dcc
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-08-19 19:45:01 +0000

    Betty check

--- monty.h
+++ monty.h
@@ -21,9 +21,9 @@
  */
 typedef struct stack_s
 {
-  int n;
-  struct stack_s *prev;
-  struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /*-Instruction Structure Data*/
@@ -37,8 +37,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-  char *opcode;
-  void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /*-Global Variable Structure Data*/
@@ -50,9 +50,9 @@ typedef struct instruction_s
  */
 struct GlobVar
 {
-  FILE *InputF;
-  char *TMP;
-  int Mode;
+	FILE *InputF;
+	char *TMP;
+	int Mode;
 };
 
 extern struct GlobVar GV;
