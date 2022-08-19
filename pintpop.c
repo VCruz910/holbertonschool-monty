6725f81c5841ba93f3fcf9f9264ccc44015d6e11
VCruz910 <vscruz910@gmail.com>
2022-08-19 18:42:47 +0000

    Fixed code errors to compile it properly

--- pintpop.c
+++ pintpop.c
@@ -1,38 +1,40 @@
 #include "monty.h"
 /**
- * pint - prints a value at the top of the stack
+ * pint - Prints a value at the top of the stack
  * @stack: doubly linked list
- * @Value: ...
+ * @Val: ...
  *
  **/
-void pint(stack_t **stack, unsigned int Value)
+void pint(stack_t **stack, unsigned int Val)
 {
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%i: can't pint, stack empty\n", value);
-			exit(EXIT_FAILURE);
+		printf("L%i: can't pint, stack empty\n", Val);
+		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
 }
 /**
- * pop - removes the top element of the stack
- * @stack: head of the doubly linked list
- * @Value: ...
+ * pop - Removes the top element of the stack
+ * @stack: Head of the doubly linked list
+ * @Val: ...
  *
  **/
-void pop(stack_t **stack, unsigned int Value)
+void pop(stack_t **stack, unsigned int Val)
 {
 	stack_t  *TMP;
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", Value);
-		freedlist(*stack);
+		printf("L%d: can't pop an empty stack\n", Val);
+		freelist(*stack);
 		exit(EXIT_FAILURE);
 	}
 	TMP = *stack;
-		(*stack) = TMP->next;
-		if (*stack != NULL)
-			(*stack)->prev = NULL;
-		free(TMP);
+	(*stack) = TMP->next;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
+	free(TMP);
 }
