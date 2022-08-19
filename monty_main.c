287b97922ace6a1a21ae136e9065195a9a9c5843
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-08-19 19:44:37 +0000

    Trying to fix some checkers

--- monty_main.c
+++ monty_main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		fprintf(stdout, "USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	MOpenFile(argv[1], &MHead);
